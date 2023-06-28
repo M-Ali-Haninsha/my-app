23c092d7a3377dec597312409d8f168467443af3
M-Ali-Haninsha <alianinsha@gmail.com>
2023-06-28 13:51:16 +0530

    toDoList

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,15 @@
+import {Task} from './Task'
+export const TASKS : Task[]= [
+    {
+    id: 1,
+    text: 'morning walk',
+    day: 'May 5th 7:00am',
+    reminder: true
+    },
+    {
+    id: 2,
+    text: 'Meeting at School',
+    day: 'May 6th 1:00pm',
+    reminder: true    
+    }
+]
